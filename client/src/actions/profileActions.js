ab8504a6facf3d7269a3cf71e3ab80e8126f9c73
Shubalub Kingz <atstay@gmail.com>
2019-03-27 10:57:43 -0600

    profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,79 +1,108 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER  } from './types';
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  SET_CURRENT_USER
+} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
-    dispatch(setProfileLoading());
-    axios.get('/api/profile')
-    .then(res => 
-        dispatch({
-            type: GET_PROFILE,
-            payload: res.data
-        })
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
     )
-    .catch(err => 
-        dispatch({
-            type: GET_PROFILE,
-            payload: {}
-        })
-    )
-}
-
-//create profile 
-export const createProfile= (profileData, history) => dispatch => {
-    axios
-        .post('/api/profile', profileData)
-        .then(res => history.push('/dashboard'))
-        .catch(err =>
-            dispatch({
-                type: GET_ERRORS,
-                payload: err.response.data
-            })
-        );
-}
-
-// add experience 
-export const addExperience = (expData, history) => dispatch => {
-    axios 
-        .post('/api/profile/experience', expData)
-        .then(res=> history.push('/dashboard'))
-        .catch(err => dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-        })
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
     );
-}
+};
+
+//create profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 //Delete account and profile
 export const deleteAccount = () => dispatch => {
-    if(window.confirm('Are you sure? CAN NOT BE UNDONE')){
-        axios
-            .delete('/api/profile')
-            .then(res => 
-                dispatch({
-                    type: SET_CURRENT_USER,
-                    payload: {}
-                })
-            ).catch(err => 
-                dispatch({
-                    type: GET_ERRORS,
-                    payload: err.response.data
-                })
-            );
-    }
-}
+  if (window.confirm("Are you sure? CAN NOT BE UNDONE")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
+};
 
-//Profile Loading 
+//Profile Loading
 export const setProfileLoading = () => {
-    return{
-        type: PROFILE_LOADING
-    }
-}
+  return {
+    type: PROFILE_LOADING
+  };
+};
 
 // clear profile
 export const clearCurrentProfile = () => {
-    return{
-        type: CLEAR_CURRENT_PROFILE        
-    }
-}
+  return {
+    type: CLEAR_CURRENT_PROFILE
+  };
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    );
+};
---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import {
+  getProfiles,
+  getCurrentProfile,
+  deleteAccount
+} from "../../actions/profileActions";
+import Spinner from "../common/Spinner";
+// import ProfileActions from "./ProfileActions";
+import Card from "../Card/Card";
+import Container from "../Card/Container";
+import Axios from "axios";
+
+class Profiles extends Component {
+  componentDidMount() {
+    this.props.getProfiles();
+  }
+  render() {
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
+
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profiles.length > 0) {
+        profileItems = <h1>WELCOME TO THE SHIT EATING CLUB</h1>;
+      } else {
+        profileItems = <h4>No profiles found...</h4>;
+      }
+    }
+    return (
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <p className="lead text-center">
+                Browse and connect with musicians and artists
+              </p>
+              {profileItems}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
