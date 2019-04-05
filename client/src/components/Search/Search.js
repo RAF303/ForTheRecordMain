49b6b2ccd86b1b3474f335d22099155bc92ee00a
Rafael Rodriguez <rafaelrodriguez@Rafaels-MacBook-Pro.local>
2019-04-04 18:45:41 -0600

    search working

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -3,15 +3,27 @@ import TextFieldGroup from "../common/TextFieldGroup";
 import SearchResultsContainer from "../SearchResultsContainer/SearchResultsContainer";
 import Card from "../Card/Card";
 import axios from "axios";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import ProfileItem2 from "../profiles2/ProfileItem2";
+import { getCurrentProfile} from '../../actions/profileActions'
+import Profile from '../profile/Profile'
 
 class Search extends Component {
   state = {
     search: "",
     users: [],
-    results: []
+    results: [],
+    profileInfo: {
+      name: "",
+      location: "",
+      skills: [],
+      instrument: ""
+    }
   };
 
   componentDidMount() {
+    this.props.getCurrentProfile();
     axios.get("api/profile/all").then(res => {
       let users = [];
       res.data.map(item => {
@@ -31,21 +43,57 @@ class Search extends Component {
 
   onClick = e => {
     let { users, search, results } = this.state;
-    let matches = [];
+  
     e.preventDefault();
 
     users.map((user, i) => {
       if (user.status.toLowerCase() == search.toLowerCase()) {
-        matches.push(user);
+    let name, location, skills, instrument;
+
+    name = user.user.name;
+    location = user.location;
+    skills = user.skills;
+    instrument = user.status;
+
+    console.log(name, location, skills, instrument);
+
+    this.setState({
+      profileInfo: {
+        name,
+        location,
+        skills,
+        instrument
       }
     });
 
-    this.setState({
-      results: matches
+    console.log(this.state.profileInfo)
+        results.push(user);
+      }
     });
+
+    // this.setState({
+    //   results: matches
+    // });
+    // e.preventDefault();
+    const {profile} = this.props.profile;
+    console.log(this.state.users);
+    // console.log('user', user);
+    // console.log('search', search);
+    // console.log('results', results);
+    // console.log(profile);
+
+    
+
+    // const matche= {
+    //   instrument : user.status,
+    // }
   };
 
   render() {
+    // const { user } = this.props.auth;
+    // const { profile } = this.props.profile;
+    const {name, location, skills, instrument } = this.state.profileInfo;
+    
     return (
       <div>
         <TextFieldGroup
@@ -58,13 +106,7 @@ class Search extends Component {
         <button onClick={this.onClick}>Search</button>
         <SearchResultsContainer>
           {this.state.results.map((result, i) => {
-            return (
-              <Card
-                key={i}
-                name={result.user.name}
-                instrument={result.status}
-              />
-            );
+            return <ProfileItem2 avatar={"avatar"} name={result.user.name} skills={result.skills} location={result.location} key={i}/>;
           })}
         </SearchResultsContainer>
       </div>
@@ -72,4 +114,15 @@ class Search extends Component {
   }
 }
 
-export default Search;
+Search.propTypes = {
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {getCurrentProfile})(Search);
