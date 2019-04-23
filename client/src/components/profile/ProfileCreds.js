cf277a1d640bf94dccafd3ba18fc358ef7ea2553
Rafael Rodriguez <rafaelrodriguez@Rafaels-MacBook-Pro.local>
2019-04-23 14:41:30 -0600

    just needs polish

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -29,12 +29,6 @@ class ProfileCreds extends Component {
     return (
       <div className="row">
         <div className="col-md-6">
-          <h3 className="text-center text-info">Experience</h3>
-          {expItems.length > 0 ? (
-            <ul className="list-group">{expItems}</ul>
-          ) : (
-            <p className="text-center">No Experience Listed</p>
-          )}
         </div>
       </div>
     );
